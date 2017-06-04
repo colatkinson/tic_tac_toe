4a1657483aa650b313fa072eafc973a712b6ec9b
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-06-04 04:06:07 -0400

    Add script to generate all possible documents

---
+++ gen_all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./build/tic-tac-toe -o 1p_electronic.pdf;
+./build/tic-tac-toe -2 -o 2p_electronic.pdf;
+./build/tic-tac-toe -p -o 1p_print.pdf;
+./build/tic-tac-toe -2p -o 2p_electronic.pdf;

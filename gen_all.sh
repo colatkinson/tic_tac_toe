d6a958a395b291c579c5284d9773b037ca3ece0d
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-06-04 04:13:15 -0400

    Fix whoopsie in release script

--- gen_all.sh
+++ gen_all.sh
@@ -3,4 +3,4 @@
 ./build/tic-tac-toe -o 1p_electronic.pdf;
 ./build/tic-tac-toe -2 -o 2p_electronic.pdf;
 ./build/tic-tac-toe -p -o 1p_print.pdf;
-./build/tic-tac-toe -2p -o 2p_electronic.pdf;
+./build/tic-tac-toe -2p -o 2p_print.pdf;
